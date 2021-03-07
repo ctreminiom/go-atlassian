8e132d6d2d8092c838c11f524d755cde4eaec86a
Carlos Treminio <carlos.treminio@equifax.com>
2021-03-07 05:20:19 +0000

    :ambulance: fixed the GoLint non-used warning on the Service Management code samples

--- jira/sm/example/executor.go
+++ jira/sm/example/executor.go
@@ -1,13 +1,16 @@
 package main
 
 func main() {
-	//getServiceManagementInfo
-	//createCustomer()
-	//getOrganizations()
-	//createOrganization() //1
-	//deleteOrganization()
-	//addUsersToOrganization()
-	//getOrganizationUsers()
-	//removeUsersOrganization()
+	getServiceManagementInfo()
+	createCustomer()
+	getOrganizations()
+	createOrganization() //1
+	deleteOrganization()
+	addUsersToOrganization()
+	getOrganizationUsers()
+	removeUsersOrganization()
 	projectOrganization()
+	associate()
+	getKnowledgebaseArticles()
+	detachOrganization()
 }

a1d4306440efcdbb7ae2970611fae98614a9e31a
Carlos Treminio <ctreminiom079@gmail.com>
2021-04-13 12:23:10 -0600

    :recycle: refactor IssueTypeService with the correct struct types and documented the examples

---
+++ jira/examples/issue-type/update/update.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"github.com/ctreminiom/go-atlassian/jira"
+	"log"
+	"os"
+)
+
+func main() {
+
+	/*
+		----------- Set an environment variable in git bash -----------
+		export HOST="https://ctreminiom.atlassian.net/"
+		export MAIL="MAIL_ADDRESS"
+		export TOKEN="TOKEN_API"
+
+		Docs: https://stackoverflow.com/questions/34169721/set-an-environment-variable-in-git-bash
+	*/
+
+	var (
+		host  = os.Getenv("HOST")
+		mail  = os.Getenv("MAIL")
+		token = os.Getenv("TOKEN")
+	)
+
+	atlassian, err := jira.New(nil, host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	atlassian.Auth.SetBasicAuth(mail, token)
+
+	issueTypePayload := jira.IssueTypePayloadScheme{
+		Name:        "Risk UPDATED",
+		Description: "this is the issue type description, UPDATED",
+	}
+
+	issueType, response, err := atlassian.Issue.Type.Update(context.Background(), "id", &issueTypePayload)
+	if err != nil {
+		if response != nil {
+			log.Println("Response HTTP Response", string(response.BodyAsBytes))
+		}
+		log.Fatal(err)
+	}
+
+	log.Println("Response HTTP Code", response.StatusCode)
+	log.Println("HTTP Endpoint Used", response.Endpoint)
+	log.Println(issueType.Name)
+}

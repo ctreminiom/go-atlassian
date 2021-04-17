9dd00e676e66f81d1ab8476a11c88c76e41531cd
Carlos Treminio <ctreminiom079@gmail.com>
2021-04-17 16:45:19 -0600

    :recycle: refactor ProjectComponentService with the correct struct types, and documented the examples.

---
+++ jira/examples/project-component/gets/gets.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"github.com/ctreminiom/go-atlassian/jira"
+	"log"
+	"os"
+)
+
+func main() {
+
+	/*
+		----------- Set an environment variable in git bash -----------
+		export HOST="https://ctreminiom.atlassian.net/"
+		export MAIL="MAIL_ADDRESS"
+		export TOKEN="TOKEN_API"
+
+		Docs: https://stackoverflow.com/questions/34169721/set-an-environment-variable-in-git-bash
+	*/
+
+	var (
+		host  = os.Getenv("HOST")
+		mail  = os.Getenv("MAIL")
+		token = os.Getenv("TOKEN")
+	)
+
+	atlassian, err := jira.New(nil, host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	atlassian.Auth.SetBasicAuth(mail, token)
+
+	var projectKey = "KP"
+	components, response, err := atlassian.Project.Component.Gets(context.Background(), projectKey)
+	if err != nil {
+		if response != nil {
+			log.Println("Response HTTP Response", string(response.BodyAsBytes))
+		}
+		log.Fatal(err)
+	}
+
+	for _, component := range *components {
+		log.Println(component)
+	}
+}
---
+++ jira/examples/project-component/update/update.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/ctreminiom/go-atlassian/jira"
+	"log"
+	"os"
+)
+
+func main() {
+
+	/*
+		----------- Set an environment variable in git bash -----------
+		export HOST="https://ctreminiom.atlassian.net/"
+		export MAIL="MAIL_ADDRESS"
+		export TOKEN="TOKEN_API"
+
+		Docs: https://stackoverflow.com/questions/34169721/set-an-environment-variable-in-git-bash
+	*/
+
+	var (
+		host  = os.Getenv("HOST")
+		mail  = os.Getenv("MAIL")
+		token = os.Getenv("TOKEN")
+	)
+
+	atlassian, err := jira.New(nil, host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	atlassian.Auth.SetBasicAuth(mail, token)
+
+	payload := &jira.ProjectComponentPayloadScheme{
+		IsAssigneeTypeValid: false,
+		Name:                "Component 1 - UPDATED",
+		Description:         "This is a Jira component - UPDATED",
+	}
+
+	componentUpdated, response, err := atlassian.Project.Component.Update(context.Background(), "10006", payload)
+	if err != nil {
+		if response != nil {
+			log.Println("Response HTTP Response", string(response.BodyAsBytes))
+		}
+		log.Fatal(err)
+	}
+
+	log.Println(componentUpdated)
+}

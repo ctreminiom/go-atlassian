21ed55a271ee0a56757eec113548f90536c84f2b
Carlos Treminio <ctreminiom079@gmail.com>
2021-06-26 20:38:07 -0600

    :sparkles:  Added attachment Create method
    Added the ability create attachments

--- confluence/contentAttachments.go
+++ confluence/contentAttachments.go
@@ -129,6 +129,68 @@ func (c *ContentAttachmentService) CreateOrUpdate(ctx context.Context, attachmen
 	return
 }
 
+// Create adds an attachment to a piece of content.
+// This method only adds a new attachment.
+// If you want to update an existing attachment, use Create or update attachments.
+func (c *ContentAttachmentService) Create(ctx context.Context, attachmentID, status, fileName string, file io.Reader) (result *ContentPageScheme, response *ResponseScheme, err error) {
+
+	if len(attachmentID) == 0 {
+		return nil, nil, notAttachmentIDError
+	}
+
+	if len(fileName) == 0 {
+		return nil, nil, notFileNameError
+	}
+
+	if file == nil {
+		return nil, nil, notReaderError
+	}
+
+	query := url.Values{}
+	if len(status) != 0 {
+		query.Add("status", status)
+	}
+
+	var endpoint strings.Builder
+	endpoint.WriteString(fmt.Sprintf("/wiki/rest/api/content/%v/child/attachment", attachmentID))
+
+	if query.Encode() != "" {
+		endpoint.WriteString(fmt.Sprintf("?%v", query.Encode()))
+	}
+
+	var (
+		body = &bytes.Buffer{}
+		attachmentWriter = multipart.NewWriter(body)
+	)
+
+	// create the attachment form row
+	part, _ := attachmentWriter.CreateFormFile("file", fileName)
+
+	// add the attachment metadata
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	_ = attachmentWriter.WriteField("minorEdit", "true")
+	attachmentWriter.Close()
+
+	request, err := c.client.newRequest(ctx, http.MethodPost, endpoint.String(), body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	request.Header.Add("Content-Type", attachmentWriter.FormDataContentType())
+	request.Header.Set("X-Atlassian-Token", "no-check")
+
+	response, err = c.client.Call(request, &result)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return
+}
+
 var (
 	notAttachmentIDError = fmt.Errorf("error, the attachment ID is required, please provide a valid value")
 	notFileNameError = fmt.Errorf("error, the fileName is required, please provide a valid value")
--- confluence/contentAttachments_test.go
+++ confluence/contentAttachments_test.go
@@ -438,3 +438,226 @@ func TestContentAttachmentService_CreateOrUpdate(t *testing.T) {
 	}
 
 }
+
+func TestContentAttachmentService_Create(t *testing.T) {
+
+	absolutePathMocked, err := filepath.Abs("./mocks/mock.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fileMocked, err := os.Open(absolutePathMocked)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fileErrorMocked, err := os.OpenFile(absolutePathMocked, os.O_WRONLY, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testCases := []struct {
+		name                 string
+		attachmentID, status string
+		fileName             string
+		file                 io.Reader
+		mockFile             string
+		wantHTTPMethod       string
+		endpoint             string
+		context              context.Context
+		wantHTTPCodeReturn   int
+		wantErr              bool
+	}{
+		{
+			name:               "CreateAttachmentWhenTheParametersAreCorrect",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            false,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheReaderIsBlocked",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileErrorMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheAttachmentIDIsNotProvided",
+			attachmentID:       "",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheFileNameIsNotProvided",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheFileReaderIsNotProvided",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               nil,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheContextIsNotProvided",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            nil,
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheRequestMethodIsIncorrect",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodConnect,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheStatusCodeIsIncorrect",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/get-content-attachments.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusBadGateway,
+			wantErr:            true,
+		},
+
+		{
+			name:               "CreateAttachmentWhenTheResponseBodyIsEmpty",
+			attachmentID:       "490949439393",
+			status:             "current",
+			fileName:           "mock.png",
+			file:               fileMocked,
+			mockFile:           "./mocks/empty-json.json",
+			wantHTTPMethod:     http.MethodPost,
+			endpoint:           "/wiki/rest/api/content/490949439393/child/attachment?status=current",
+			context:            context.Background(),
+			wantHTTPCodeReturn: http.StatusOK,
+			wantErr:            true,
+		},
+	}
+
+	for _, testCase := range testCases {
+
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			//Init a new HTTP mock server
+			mockOptions := mockServerOptions{
+				Endpoint:           testCase.endpoint,
+				MockFilePath:       testCase.mockFile,
+				MethodAccepted:     testCase.wantHTTPMethod,
+				ResponseCodeWanted: testCase.wantHTTPCodeReturn,
+			}
+
+			mockServer, err := startMockServer(&mockOptions)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer mockServer.Close()
+
+			//Init the library instance
+			mockClient, err := startMockClient(mockServer.URL)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			service := &ContentAttachmentService{client: mockClient}
+
+			gotResult, gotResponse, err := service.Create(testCase.context, testCase.attachmentID, testCase.status, testCase.fileName, testCase.file)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.Error(t, err)
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, gotResponse, nil)
+				assert.NotEqual(t, gotResult, nil)
+
+				apiEndpoint, err := url.Parse(gotResponse.Endpoint)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				var endpointToAssert string
+
+				if apiEndpoint.Query().Encode() != "" {
+					endpointToAssert = fmt.Sprintf("%v?%v", apiEndpoint.Path, apiEndpoint.Query().Encode())
+				} else {
+					endpointToAssert = apiEndpoint.Path
+				}
+
+				t.Logf("HTTP Endpoint Wanted: %v, HTTP Endpoint Returned: %v", testCase.endpoint, endpointToAssert)
+				assert.Equal(t, testCase.endpoint, endpointToAssert)
+			}
+
+		})
+
+	}
+
+}

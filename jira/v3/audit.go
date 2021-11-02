e9d1061e8aeff393ffd09c8ce34655be9f6b0210
Carlos Treminio <ctreminiom079@gmail.com>
2021-11-01 18:23:29 -0600

    :art: Moved the jira-audit struts to the common model package
    1. Moved the jira-audit models and errors to the common infra/model package

--- jira/v3/audit.go
+++ jira/v3/audit.go
@@ -3,6 +3,7 @@ package v3
 import (
 	"context"
 	"fmt"
+	models "github.com/ctreminiom/go-atlassian/internal/infra/models/jira"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -10,49 +11,6 @@ import (
 )
 
 type AuditService struct{ client *Client }
-
-type AuditRecordPageScheme struct {
-	Offset  int                  `json:"offset,omitempty"`
-	Limit   int                  `json:"limit,omitempty"`
-	Total   int                  `json:"total,omitempty"`
-	Records []*AuditRecordScheme `json:"records,omitempty"`
-}
-
-type AuditRecordScheme struct {
-	ID              int                                `json:"id,omitempty"`
-	Summary         string                             `json:"summary,omitempty"`
-	RemoteAddress   string                             `json:"remoteAddress,omitempty"`
-	AuthorKey       string                             `json:"authorKey,omitempty"`
-	Created         string                             `json:"created,omitempty"`
-	Category        string                             `json:"category,omitempty"`
-	EventSource     string                             `json:"eventSource,omitempty"`
-	Description     string                             `json:"description,omitempty"`
-	ObjectItem      *AuditRecordObjectItemScheme       `json:"objectItem,omitempty"`
-	ChangedValues   []*AuditRecordChangedValueScheme   `json:"changedValues,omitempty"`
-	AssociatedItems []*AuditRecordAssociatedItemScheme `json:"associatedItems,omitempty"`
-}
-
-type AuditRecordObjectItemScheme struct {
-	ID         string `json:"id,omitempty"`
-	Name       string `json:"name,omitempty"`
-	TypeName   string `json:"typeName,omitempty"`
-	ParentID   string `json:"parentId,omitempty"`
-	ParentName string `json:"parentName,omitempty"`
-}
-
-type AuditRecordChangedValueScheme struct {
-	FieldName   string `json:"fieldName,omitempty"`
-	ChangedFrom string `json:"changedFrom,omitempty"`
-	ChangedTo   string `json:"changedTo,omitempty"`
-}
-
-type AuditRecordAssociatedItemScheme struct {
-	ID         string `json:"id,omitempty"`
-	Name       string `json:"name,omitempty"`
-	TypeName   string `json:"typeName,omitempty"`
-	ParentID   string `json:"parentId,omitempty"`
-	ParentName string `json:"parentName,omitempty"`
-}
 
 type AuditRecordGetOptions struct {
 	Filter   string
@@ -62,7 +20,7 @@ type AuditRecordGetOptions struct {
 // Get returns a list of audit records. The list can be filtered to include items:
 // Docs: https://docs.go-atlassian.io/jira-software-cloud/audit-records#get-audit-records
 // Official Docs: https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-audit-records/#api-rest-api-3-auditing-record-get
-func (a *AuditService) Get(ctx context.Context, options *AuditRecordGetOptions, offset, limit int) (result *AuditRecordPageScheme, response *ResponseScheme, err error) {
+func (a *AuditService) Get(ctx context.Context, options *AuditRecordGetOptions, offset, limit int) (result *models.AuditRecordPageScheme, response *ResponseScheme, err error) {
 
 	params := url.Values{}
 	params.Add("offset", strconv.Itoa(offset))

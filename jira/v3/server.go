23418701f16560cba7ad2db33894e1e82e493231
Carlos Treminio <ctreminiom079@gmail.com>
2021-11-03 10:36:51 -0600

    :building_construction: Moved the jira-server structs to the common model package
    1. Moved the jira-server models and errors to the common infra/model package
    
    2. Added the custom errors

--- jira/v3/server.go
+++ jira/v3/server.go
@@ -2,32 +2,16 @@ package v3
 
 import (
 	"context"
+	models "github.com/ctreminiom/go-atlassian/pkg/infra/models/jira"
 	"net/http"
 )
 
 type ServerService struct{ client *Client }
 
-type ServerInformationScheme struct {
-	BaseURL        string `json:"baseUrl"`
-	Version        string `json:"version"`
-	VersionNumbers []int  `json:"versionNumbers"`
-	DeploymentType string `json:"deploymentType"`
-	BuildNumber    int    `json:"buildNumber"`
-	BuildDate      string `json:"buildDate"`
-	ServerTime     string `json:"serverTime"`
-	ScmInfo        string `json:"scmInfo"`
-	ServerTitle    string `json:"serverTitle"`
-	HealthChecks   []struct {
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		Passed      bool   `json:"passed"`
-	} `json:"healthChecks"`
-}
-
 // Info returns information about the Jira instance.
 // Docs: https://docs.go-atlassian.io/jira-software-cloud/server#get-jira-instance-info
 // Atlassian Docs: https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-server-info/#api-rest-api-3-serverinfo-get
-func (s *ServerService) Info(ctx context.Context) (result *ServerInformationScheme, response *ResponseScheme, err error) {
+func (s *ServerService) Info(ctx context.Context) (result *models.ServerInformationScheme, response *ResponseScheme, err error) {
 
 	var endpoint = "rest/api/3/serverInfo"
 

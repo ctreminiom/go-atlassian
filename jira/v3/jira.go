ee51df11611a75886c95eca5a699e539e8ea290f
Carlos Treminio <ctreminiom079@gmail.com>
2022-03-07 15:17:48 -0600

    :sparkles: Added on the Project.Property service.
    1. Added the project property service on the Jira V3

--- jira/v3/jira.go
+++ jira/v3/jira.go
@@ -145,9 +145,10 @@ func New(httpClient *http.Client, site string) (client *Client, err error) {
 			Actor:  &ProjectRoleActorService{client: client},
 		},
 
-		Type:    &ProjectTypeService{client: client},
-		Version: &ProjectVersionService{client: client},
-		Feature: &ProjectFeatureService{client: client},
+		Type:     &ProjectTypeService{client: client},
+		Version:  &ProjectVersionService{client: client},
+		Feature:  &ProjectFeatureService{client: client},
+		Property: &ProjectPropertyService{client: client},
 	}
 
 	client.User = &UserService{

a52c1206c88c4554e714ec7906d7699b6181355a
Carlos Treminio <ctreminiom079@gmail.com>
2022-09-15 01:10:53 -0600

    :sparkles: Mapped the Jira Issue Attachment Download method #138

--- jira/internal/attachment_impl_test.go
+++ jira/internal/attachment_impl_test.go
@@ -770,3 +770,152 @@ func Test_internalIssueAttachmentServiceImpl_Add(t *testing.T) {
 		})
 	}
 }
+
+func Test_internalIssueAttachmentServiceImpl_Download(t *testing.T) {
+
+	type fields struct {
+		c       service.Client
+		version string
+	}
+
+	type args struct {
+		ctx          context.Context
+		attachmentId string
+		redirect     bool
+	}
+
+	testCases := []struct {
+		name    string
+		fields  fields
+		args    args
+		on      func(*fields)
+		wantErr bool
+		Err     error
+	}{
+		{
+			name:   "when the api version is v2",
+			fields: fields{version: "2"},
+			args: args{
+				ctx:          context.TODO(),
+				attachmentId: "1110",
+				redirect:     false,
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewClient(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/2/attachment/content/1110?redirect=false",
+					nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					nil).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+		},
+
+		{
+			name:   "when the api version is v3",
+			fields: fields{version: "3"},
+			args: args{
+				ctx:          context.TODO(),
+				attachmentId: "1110",
+				redirect:     true,
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewClient(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/3/attachment/content/1110",
+					nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					nil).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+
+			},
+		},
+
+		{
+			name:   "when the attachment id is not provided",
+			fields: fields{version: "2"},
+			args: args{
+				ctx:          context.TODO(),
+				attachmentId: "",
+			},
+			on: func(fields *fields) {
+				fields.c = mocks.NewClient(t)
+			},
+			wantErr: true,
+			Err:     model.ErrNoAttachmentIDError,
+		},
+
+		{
+			name:   "when the http request cannot be created",
+			fields: fields{version: "2"},
+			args: args{
+				ctx:          context.TODO(),
+				attachmentId: "1110",
+				redirect:     true,
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewClient(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/2/attachment/content/1110",
+					nil).
+					Return(&http.Request{}, errors.New("error, unable to create the http request"))
+
+				fields.c = client
+
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to create the http request"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			if testCase.on != nil {
+				testCase.on(&testCase.fields)
+			}
+
+			attachmentService, err := NewIssueAttachmentService(testCase.fields.c, testCase.fields.version)
+			assert.NoError(t, err)
+
+			gotResponse, err := attachmentService.Download(testCase.args.ctx, testCase.args.attachmentId, testCase.args.redirect)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.EqualError(t, err, testCase.Err.Error())
+
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, gotResponse, nil)
+			}
+
+		})
+	}
+}

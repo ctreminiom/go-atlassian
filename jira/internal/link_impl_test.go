d225afd317bdd238cd11d46dccfb25565478b7ae
Carlos Treminio <ctreminiom079@gmail.com>
2023-03-10 23:30:58 -0600

    :white_check_mark: Added the test-cases on the jira issue service

---
+++ jira/internal/link_impl_test.go
@@ -0,0 +1,62 @@
+package internal
+
+import (
+	model "github.com/ctreminiom/go-atlassian/pkg/infra/models"
+	"github.com/ctreminiom/go-atlassian/service"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_NewLinkService(t *testing.T) {
+
+	type args struct {
+		client  service.Client
+		version string
+	}
+
+	testCases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		err     error
+	}{
+		{
+			name: "when the parameters are correct",
+			args: args{
+				client:  nil,
+				version: "3",
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "when the version is not provided",
+			args: args{
+				client:  nil,
+				version: "",
+			},
+			wantErr: true,
+			err:     model.ErrNoVersionProvided,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, got1, err := NewLinkService(testCase.args.client, testCase.args.version, nil)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.EqualError(t, err, testCase.err.Error())
+
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, got, nil)
+				assert.NotEqual(t, got1, nil)
+			}
+		})
+	}
+}

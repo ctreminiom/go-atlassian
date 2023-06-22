5119561022eab01f86cdcfb0c03718bb69f66694
Carlos Treminio <ctreminiom079@gmail.com>
2023-06-21 18:17:48 -0600

    :art: Changed the client interface from Client to Connector on the service management participant service.

--- jira/sm/internal/participant_impl.go
+++ jira/sm/internal/participant_impl.go
@@ -11,15 +11,11 @@ import (
 	"strconv"
 )
 
-func NewParticipantService(client service.Client, version string) (*ParticipantService, error) {
-
-	if version == "" {
-		return nil, model.ErrNoVersionProvided
-	}
+func NewParticipantService(client service.Connector, version string) *ParticipantService {
 
 	return &ParticipantService{
 		internalClient: &internalServiceRequestParticipantImpl{c: client, version: version},
-	}, nil
+	}
 }
 
 type ParticipantService struct {
@@ -54,7 +50,7 @@ func (s *ParticipantService) Remove(ctx context.Context, issueKeyOrID string, ac
 }
 
 type internalServiceRequestParticipantImpl struct {
-	c       service.Client
+	c       service.Connector
 	version string
 }
 
@@ -70,18 +66,18 @@ func (i *internalServiceRequestParticipantImpl) Gets(ctx context.Context, issueK
 
 	endpoint := fmt.Sprintf("rest/servicedeskapi/request/%v/participant?%v", issueKeyOrID, params.Encode())
 
-	request, err := i.c.NewRequest(ctx, http.MethodGet, endpoint, nil)
+	req, err := i.c.NewRequest(ctx, http.MethodGet, endpoint, "", nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	page := new(model.RequestParticipantPageScheme)
-	response, err := i.c.Call(request, page)
+	res, err := i.c.Call(req, page)
 	if err != nil {
-		return nil, response, err
+		return nil, res, err
 	}
 
-	return page, response, nil
+	return page, res, nil
 }
 
 func (i *internalServiceRequestParticipantImpl) Add(ctx context.Context, issueKeyOrID string, accountIDs []string) (*model.RequestParticipantPageScheme, *model.ResponseScheme, error) {
@@ -94,31 +90,20 @@ func (i *internalServiceRequestParticipantImpl) Add(ctx context.Context, issueKe
 		return nil, nil, model.ErrNoAccountSliceError
 	}
 
-	payload := struct {
-		AccountIds []string `json:"accountIds"`
-	}{
-		AccountIds: accountIDs,
-	}
-
-	reader, err := i.c.TransformStructToReader(&payload)
-	if err != nil {
-		return nil, nil, err
-	}
-
 	endpoint := fmt.Sprintf("rest/servicedeskapi/request/%v/participant", issueKeyOrID)
 
-	request, err := i.c.NewRequest(ctx, http.MethodPost, endpoint, reader)
+	req, err := i.c.NewRequest(ctx, http.MethodPost, endpoint, "", map[string]interface{}{"accountIds": accountIDs})
 	if err != nil {
 		return nil, nil, err
 	}
 
 	page := new(model.RequestParticipantPageScheme)
-	response, err := i.c.Call(request, page)
+	res, err := i.c.Call(req, page)
 	if err != nil {
-		return nil, response, err
+		return nil, res, err
 	}
 
-	return page, response, nil
+	return page, res, nil
 }
 
 func (i *internalServiceRequestParticipantImpl) Remove(ctx context.Context, issueKeyOrID string, accountIDs []string) (*model.RequestParticipantPageScheme, *model.ResponseScheme, error) {
@@ -130,29 +115,19 @@ func (i *internalServiceRequestParticipantImpl) Remove(ctx context.Context, issu
 	if len(accountIDs) == 0 {
 		return nil, nil, model.ErrNoAccountSliceError
 	}
-	payload := struct {
-		AccountIds []string `json:"accountIds"`
-	}{
-		AccountIds: accountIDs,
-	}
-
-	reader, err := i.c.TransformStructToReader(&payload)
-	if err != nil {
-		return nil, nil, err
-	}
 
 	endpoint := fmt.Sprintf("rest/servicedeskapi/request/%v/participant", issueKeyOrID)
 
-	request, err := i.c.NewRequest(ctx, http.MethodDelete, endpoint, reader)
+	req, err := i.c.NewRequest(ctx, http.MethodDelete, endpoint, "", map[string]interface{}{"accountIds": accountIDs})
 	if err != nil {
 		return nil, nil, err
 	}
 
 	page := new(model.RequestParticipantPageScheme)
-	response, err := i.c.Call(request, page)
+	res, err := i.c.Call(req, page)
 	if err != nil {
-		return nil, response, err
+		return nil, res, err
 	}
 
-	return page, response, nil
+	return page, res, nil
 }

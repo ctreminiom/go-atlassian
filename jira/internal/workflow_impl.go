7038cb08ac5b5146d32a7c95ffc465f0e39a0b07
Carlos Treminio <ctreminiom079@gmail.com>
2023-07-31 11:00:59 -0600

    :sparkles: Mapped the new Jira workflow edition API's

--- jira/internal/workflow_impl.go
+++ jira/internal/workflow_impl.go
@@ -73,9 +73,154 @@ func (w *WorkflowService) Delete(ctx context.Context, workflowId string) (*model
 	return w.internalClient.Delete(ctx, workflowId)
 }
 
+// Bulk returns a list of workflows and related statuses by providing workflow names,
+//
+// workflow IDs, or project and issue types.
+//
+// POST /rest/api/{2-3}/workflows
+func (w *WorkflowService) Bulk(ctx context.Context, options *model.WorkflowBulkOptionsScheme, expand []string) (*model.WorkflowReadResponseScheme, *model.ResponseScheme, error) {
+	return w.internalClient.Bulk(ctx, options, expand)
+}
+
+// Capabilities get the list of workflow capabilities for a specific workflow using either the workflow ID, or the project and issue type ID pair.
+//
+// The response includes the scope of the workflow, defined as global/project-based,
+//
+// and a list of project types that the workflow is scoped to.
+//
+// It also includes all rules organised into their broad categories (conditions, validators, actions, triggers, screens)
+//
+// as well as the source location (Atlassian-provided, Connect, Forge).
+//
+// GET /rest/api/{2-3}/workflows/capabilities
+func (w *WorkflowService) Capabilities(ctx context.Context, workflowID, projectID, issueTypeID string) (*model.WorkflowCapabilitiesScheme, *model.ResponseScheme, error) {
+	return w.internalClient.Capabilities(ctx, workflowID, projectID, issueTypeID)
+}
+
+// Creates creates workflows and related statuses.
+//
+// POST /rest/api/{2-3}/workflows/create
+func (w *WorkflowService) Creates(ctx context.Context, payload *model.WorkflowCreatesPayloadScheme) (*model.WorkflowCreateResponseScheme, *model.ResponseScheme, error) {
+	return w.internalClient.Creates(ctx, payload)
+}
+
+// Updates updates workflows and related statuses.
+//
+// POST /rest/api/{2-3}/workflows/update
+func (w *WorkflowService) Updates(ctx context.Context, payload *model.WorkflowUpdatesPayloadScheme, expand []string) (*model.WorkflowUpdateResponseScheme, *model.ResponseScheme, error) {
+	return w.internalClient.Updates(ctx, payload, expand)
+}
+
 type internalWorkflowImpl struct {
 	c       service.Connector
 	version string
+}
+
+func (i *internalWorkflowImpl) Bulk(ctx context.Context, options *model.WorkflowBulkOptionsScheme, expand []string) (*model.WorkflowReadResponseScheme, *model.ResponseScheme, error) {
+
+	var endpoint strings.Builder
+	endpoint.WriteString(fmt.Sprintf("rest/api/%v/workflows", i.version))
+
+	if len(expand) != 0 {
+
+		params := url.Values{}
+		params.Add("expand", strings.Join(expand, ","))
+		endpoint.WriteString(fmt.Sprintf("?%v", params.Encode()))
+	}
+
+	request, err := i.c.NewRequest(ctx, http.MethodPost, endpoint.String(), "", options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	page := new(model.WorkflowReadResponseScheme)
+	response, err := i.c.Call(request, page)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return page, response, nil
+}
+
+func (i *internalWorkflowImpl) Capabilities(ctx context.Context, workflowID, projectID, issueTypeID string) (*model.WorkflowCapabilitiesScheme, *model.ResponseScheme, error) {
+
+	var endpoint strings.Builder
+	endpoint.WriteString(fmt.Sprintf("rest/api/%v/workflows/capabilities", i.version))
+
+	params := url.Values{}
+
+	if workflowID != "" {
+		params.Add("workflowId", workflowID)
+	}
+
+	if projectID != "" {
+		params.Add("projectId", projectID)
+	}
+
+	if issueTypeID != "" {
+		params.Add("issueTypeId", issueTypeID)
+	}
+
+	if params.Encode() != "" {
+		endpoint.WriteString(fmt.Sprintf("?%v", params.Encode()))
+	}
+
+	request, err := i.c.NewRequest(ctx, http.MethodGet, endpoint.String(), "", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	capabilities := new(model.WorkflowCapabilitiesScheme)
+	response, err := i.c.Call(request, capabilities)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return capabilities, response, nil
+}
+
+func (i *internalWorkflowImpl) Creates(ctx context.Context, payload *model.WorkflowCreatesPayloadScheme) (*model.WorkflowCreateResponseScheme, *model.ResponseScheme, error) {
+
+	endpoint := fmt.Sprintf("rest/api/%v/workflows/create", i.version)
+
+	request, err := i.c.NewRequest(ctx, http.MethodPost, endpoint, "", payload)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	workflows := new(model.WorkflowCreateResponseScheme)
+	response, err := i.c.Call(request, workflows)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return workflows, response, nil
+}
+
+func (i *internalWorkflowImpl) Updates(ctx context.Context, payload *model.WorkflowUpdatesPayloadScheme, expand []string) (*model.WorkflowUpdateResponseScheme, *model.ResponseScheme, error) {
+
+	var endpoint strings.Builder
+	endpoint.WriteString(fmt.Sprintf("rest/api/%v/workflows/update", i.version))
+
+	if len(expand) != 0 {
+
+		params := url.Values{}
+		params.Add("expand", strings.Join(expand, ","))
+		endpoint.WriteString(fmt.Sprintf("?%v", params.Encode()))
+	}
+
+	request, err := i.c.NewRequest(ctx, http.MethodPost, endpoint.String(), "", payload)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	workflows := new(model.WorkflowUpdateResponseScheme)
+	response, err := i.c.Call(request, workflows)
+	if err != nil {
+		return nil, response, err
+	}
+
+	return workflows, response, nil
 }
 
 func (i *internalWorkflowImpl) Create(ctx context.Context, payload *model.WorkflowPayloadScheme) (*model.WorkflowCreatedResponseScheme, *model.ResponseScheme, error) {
--- pkg/infra/models/jira_workflow.go
+++ pkg/infra/models/jira_workflow.go
@@ -107,3 +107,261 @@ type WorkflowConditionScheme struct {
 	Operator      string                     `json:"operator,omitempty"`
 	Type          string                     `json:"type,omitempty"`
 }
+
+type WorkflowBulkOptionsScheme struct {
+	ProjectAndIssueTypes []*ProjectAndIssueTypePairScheme `json:"projectAndIssueTypes,omitempty"`
+	WorkflowIds          []string                         `json:"workflowIds,omitempty"`
+	WorkflowNames        []string                         `json:"workflowNames,omitempty"`
+}
+
+type ProjectAndIssueTypePairScheme struct {
+	IssueTypeID string `json:"issueTypeId,omitempty"`
+	ProjectID   string `json:"projectId,omitempty"`
+}
+
+type WorkflowReadResponseScheme struct {
+	Statuses  []*WorkflowStatusDetailScheme `json:"statuses,omitempty"`
+	Workflows []*JiraWorkflowScheme         `json:"workflows,omitempty"`
+}
+
+type JiraWorkflowScheme struct {
+	Description      string                           `json:"description,omitempty"`
+	ID               string                           `json:"id,omitempty"`
+	IsEditable       bool                             `json:"isEditable,omitempty"`
+	Name             string                           `json:"name,omitempty"`
+	Scope            *WorkflowStatusScopeScheme       `json:"scope,omitempty"`
+	StartPointLayout *StartPointLayoutScheme          `json:"startPointLayout,omitempty"`
+	Statuses         []*WorkflowReferenceStatusScheme `json:"statuses,omitempty"`
+	TaskID           string                           `json:"taskId,omitempty"`
+	Transitions      []*WorkflowTransitionsScheme     `json:"transitions,omitempty"`
+	Usages           []*ProjectIssueTypesScheme       `json:"usages,omitempty"`
+	Version          *WorkflowDocumentVersionScheme   `json:"version,omitempty"`
+}
+
+type StartPointLayoutScheme struct {
+	X float64 `json:"x,omitempty"`
+	Y float64 `json:"y,omitempty"`
+}
+
+type WorkflowReferenceStatusScheme struct {
+	Deprecated      bool                    `json:"deprecated,omitempty"`
+	Layout          *StartPointLayoutScheme `json:"layout,omitempty"`
+	StatusReference string                  `json:"statusReference,omitempty"`
+}
+
+type WorkflowTransitionsScheme struct {
+	Actions            []*WorkflowRuleConfigurationScheme `json:"actions,omitempty"`
+	Conditions         *ConditionGroupConfigurationScheme `json:"conditions,omitempty"`
+	CustomIssueEventID string                             `json:"customIssueEventId,omitempty"`
+	Description        string                             `json:"description,omitempty"`
+	From               []*WorkflowStatusAndPortScheme     `json:"from,omitempty"`
+	ID                 string                             `json:"id,omitempty"`
+	Name               string                             `json:"name,omitempty"`
+	To                 *WorkflowStatusAndPortScheme       `json:"to,omitempty"`
+	TransitionScreen   *WorkflowRuleConfigurationScheme   `json:"transitionScreen,omitempty"`
+	Triggers           []*WorkflowTriggerScheme           `json:"triggers,omitempty"`
+	Type               string                             `json:"type,omitempty"`
+	Validators         []*WorkflowRuleConfigurationScheme `json:"validators,omitempty"`
+}
+
+type WorkflowRuleConfigurationScheme struct {
+	ID      string `json:"id,omitempty"`
+	RuleKey string `json:"ruleKey,omitempty"`
+}
+
+type ConditionGroupConfigurationScheme struct {
+	ConditionGroups []*ConditionGroupConfigurationScheme `json:"conditionGroups,omitempty"`
+	Conditions      []*WorkflowRuleConfigurationScheme   `json:"conditions,omitempty"`
+	Operation       string                               `json:"operation,omitempty"`
+}
+
+type WorkflowStatusAndPortScheme struct {
+	Port            int    `json:"port,omitempty"`
+	StatusReference string `json:"statusReference,omitempty"`
+}
+
+type WorkflowTriggerScheme struct {
+	ID      string `json:"id,omitempty"`
+	RuleKey string `json:"ruleKey,omitempty"`
+}
+
+type WorkflowDocumentVersionScheme struct {
+	ID            string `json:"id,omitempty"`
+	VersionNumber int    `json:"versionNumber,omitempty"`
+}
+
+type WorkflowCapabilitiesScheme struct {
+	ConnectRules []*AvailableWorkflowConnectRuleScheme `json:"connectRules,omitempty"`
+	EditorScope  string                                `json:"editorScope,omitempty"`
+	ForgeRules   []*AvailableWorkflowForgeRuleScheme   `json:"forgeRules,omitempty"`
+	ProjectTypes []string                              `json:"projectTypes,omitempty"`
+	SystemRules  []*AvailableWorkflowSystemRuleScheme  `json:"systemRules,omitempty"`
+	TriggerRules []*AvailableWorkflowTriggersScheme    `json:"triggerRules,omitempty"`
+}
+
+type AvailableWorkflowConnectRuleScheme struct {
+	AddonKey    string `json:"addonKey,omitempty"`
+	CreateURL   string `json:"createUrl,omitempty"`
+	Description string `json:"description,omitempty"`
+	EditURL     string `json:"editUrl,omitempty"`
+	ModuleKey   string `json:"moduleKey,omitempty"`
+	Name        string `json:"name,omitempty"`
+	RuleKey     string `json:"ruleKey,omitempty"`
+	RuleType    string `json:"ruleType,omitempty"`
+	ViewURL     string `json:"viewUrl,omitempty"`
+}
+
+type AvailableWorkflowForgeRuleScheme struct {
+	Description string `json:"description,omitempty"`
+	ID          string `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	RuleKey     string `json:"ruleKey,omitempty"`
+	RuleType    string `json:"ruleType,omitempty"`
+}
+
+type AvailableWorkflowSystemRuleScheme struct {
+	Description                     string   `json:"description,omitempty"`
+	IncompatibleRuleKeys            []string `json:"incompatibleRuleKeys,omitempty"`
+	IsAvailableForInitialTransition bool     `json:"isAvailableForInitialTransition,omitempty"`
+	IsVisible                       bool     `json:"isVisible,omitempty"`
+	Name                            string   `json:"name,omitempty"`
+	RuleKey                         string   `json:"ruleKey,omitempty"`
+	RuleType                        string   `json:"ruleType,omitempty"`
+}
+
+type AvailableWorkflowTriggersScheme struct {
+	AvailableTypes []*AvailableWorkflowTriggerTypesScheme `json:"availableTypes,omitempty"`
+	RuleKey        string                                 `json:"ruleKey,omitempty"`
+}
+
+type AvailableWorkflowTriggerTypesScheme struct {
+	Description string `json:"description,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Type        string `json:"type,omitempty"`
+}
+
+type WorkflowCreatesPayloadScheme struct {
+	Scope     *WorkflowStatusScopeScheme     `json:"scope,omitempty"`
+	Statuses  []*WorkflowStatusUpdateScheme  `json:"statuses,omitempty"`
+	Workflows []*WorkflowCreatePayloadScheme `json:"workflows,omitempty"`
+}
+
+// WorkflowStatusUpdateScheme contains the details of the status being updated.
+type WorkflowStatusUpdateScheme struct {
+	Description     string `json:"description,omitempty"`
+	ID              string `json:"id,omitempty"`
+	Name            string `json:"name,omitempty"`
+	StatusCategory  string `json:"statusCategory,omitempty"`
+	StatusReference string `json:"statusReference,omitempty"`
+}
+
+type WorkflowCreatePayloadScheme struct {
+	Description      string                      `json:"description,omitempty"`
+	Name             string                      `json:"name,omitempty"`
+	StartPointLayout *StartPointLayoutScheme     `json:"startPointLayout,omitempty"`
+	Statuses         []*StatusLayoutUpdateScheme `json:"statuses,omitempty"`
+	Transitions      []*TransitionUpdateScheme   `json:"transitions"`
+}
+
+type StatusLayoutUpdateScheme struct {
+	Layout          *StartPointLayoutScheme `json:"layout,omitempty"`
+	StatusReference string                  `json:"statusReference,omitempty"`
+}
+
+type TransitionUpdateScheme struct {
+	Actions            []*WorkflowRuleConfigurationScheme `json:"actions,omitempty"`
+	Conditions         *ConditionGroupUpdateScheme        `json:"conditions,omitempty"`
+	CustomIssueEventID string                             `json:"customIssueEventId,omitempty"`
+	Description        string                             `json:"description,omitempty"`
+	From               []*StatusReferenceAndPortScheme    `json:"from,omitempty"`
+	ID                 string                             `json:"id,omitempty"`
+	Name               string                             `json:"name,omitempty"`
+	To                 *StatusReferenceAndPortScheme      `json:"to,omitempty"`
+	TransitionScreen   *WorkflowRuleConfigurationScheme   `json:"transitionScreen,omitempty"`
+	Triggers           []*WorkflowTriggerScheme           `json:"triggers,omitempty"`
+	Type               string                             `json:"type,omitempty"`
+	Validators         []*WorkflowRuleConfigurationScheme `json:"validators,omitempty"`
+}
+
+type ConditionGroupUpdateScheme struct {
+	ConditionGroups []*ConditionGroupUpdateScheme      `json:"conditionGroups,omitempty"`
+	Conditions      []*WorkflowRuleConfigurationScheme `json:"conditions,omitempty"`
+	Operation       string                             `json:"operation,omitempty"`
+}
+
+type StatusReferenceAndPortScheme struct {
+	Port            int    `json:"port,omitempty"`
+	StatusReference string `json:"statusReference,omitempty"`
+}
+
+type WorkflowCreateResponseScheme struct {
+	Statuses  []*WorkflowStatusDetailScheme `json:"statuses,omitempty"`
+	Workflows []*JiraWorkflowScheme         `json:"workflows,omitempty"`
+}
+
+type WorkflowUpdatesPayloadScheme struct {
+	Statuses  []*WorkflowStatusUpdateScheme  `json:"statuses,omitempty"`
+	Workflows []*WorkflowUpdatePayloadScheme `json:"workflows,omitempty"`
+}
+
+type WorkflowUpdatePayloadScheme struct {
+	DefaultStatusMappings []*StatusMigrationScheme       `json:"defaultStatusMappings,omitempty"`
+	Description           string                         `json:"description,omitempty"`
+	ID                    string                         `json:"id,omitempty"`
+	StartPointLayout      *StartPointLayoutScheme        `json:"startPointLayout,omitempty"`
+	StatusMappings        []*StatusMappingScheme         `json:"statusMappings,omitempty"`
+	Statuses              []*StatusLayoutUpdateScheme    `json:"statuses,omitempty"`
+	Transitions           []*TransitionUpdateScheme      `json:"transitions,omitempty"`
+	Version               *WorkflowDocumentVersionScheme `json:"version,omitempty"`
+}
+
+type StatusMigrationScheme struct {
+	NewStatusReference string `json:"newStatusReference,omitempty"`
+	OldStatusReference string `json:"oldStatusReference,omitempty"`
+}
+
+type StatusMappingScheme struct {
+	IssueTypeID      string                   `json:"issueTypeId,omitempty"`
+	ProjectID        string                   `json:"projectId,omitempty"`
+	StatusMigrations []*StatusMigrationScheme `json:"statusMigrations,omitempty"`
+}
+
+type WorkflowUpdateResponseScheme struct {
+	Statuses  []*WorkflowStatusDetailScheme `json:"statuses,omitempty"`
+	TaskID    string                        `json:"taskId,omitempty"`
+	Workflows []*JiraWorkflowScheme         `json:"workflows,omitempty"`
+}
+
+type WorkflowCreateValidatorPayloadScheme struct {
+	Payload           *WorkflowCreatesPayloadScheme     `json:"payload,omitempty"`
+	ValidationOptions *ValidationOptionsForCreateScheme `json:"validationOptions,omitempty"`
+}
+
+type WorkflowUpdateValidatorPayloadScheme struct {
+	Payload           *WorkflowUpdatesPayloadScheme     `json:"payload,omitempty"`
+	ValidationOptions *ValidationOptionsForCreateScheme `json:"validationOptions,omitempty"`
+}
+
+type ValidationOptionsForCreateScheme struct {
+	Levels []string `json:"levels,omitempty"`
+}
+
+type WorkflowValidationErrorListScheme struct {
+	Errors []*WorkflowValidationErrorScheme `json:"errors,omitempty"`
+}
+
+type WorkflowValidationErrorScheme struct {
+	Code             string                          `json:"code,omitempty"`
+	ElementReference *WorkflowElementReferenceScheme `json:"elementReference,omitempty"`
+	Level            string                          `json:"level,omitempty"`
+	Message          string                          `json:"message,omitempty"`
+	Type             string                          `json:"type,omitempty"`
+}
+
+type WorkflowElementReferenceScheme struct {
+	PropertyKey            string                         `json:"propertyKey,omitempty"`
+	RuleID                 string                         `json:"ruleId,omitempty"`
+	StatusMappingReference *ProjectAndIssueTypePairScheme `json:"statusMappingReference,omitempty"`
+	StatusReference        string                         `json:"statusReference,omitempty"`
+	TransitionID           string                         `json:"transitionId,omitempty"`
+}

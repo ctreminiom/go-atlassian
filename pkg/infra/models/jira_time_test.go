00b8cea5fbae0df823180a029307b6babd69f511
Florian Kinder <florian.kinder@enthus.de>
2024-07-01 15:08:54 +0900

    Fixed tests

--- pkg/infra/models/jira_time_test.go
+++ pkg/infra/models/jira_time_test.go
@@ -8,6 +8,81 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDateScheme_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name    string
+		t       DateScheme
+		want    []byte
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name:    "random time",
+			t:       DateScheme(time.Date(2021, 6, 22, 7, 34, 41, 123, time.UTC)),
+			want:    []byte(`2021-06-22`),
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "timezone",
+			t:       DateScheme(time.Date(2021, 6, 22, 7, 34, 41, 123, time.FixedZone("UTC+2", 7200))),
+			want:    []byte(`2021-06-22`),
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.t.MarshalJSON()
+			if !tt.wantErr(t, err, fmt.Sprintf("MarshalJSON()")) {
+				return
+			}
+			assert.Equalf(t, string(tt.want), string(got), "MarshalJSON()")
+		})
+	}
+}
+
+func TestDateScheme_UnmarshalJSON(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    DateScheme
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "null",
+			args: args{
+				data: []byte(`null`),
+			},
+			want:    DateScheme{},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "random date",
+			args: args{
+				data: []byte(`"2021-06-22"`),
+			},
+			want:    DateScheme(time.Date(2021, 6, 22, 0, 0, 0, 0, time.UTC)),
+			wantErr: assert.NoError,
+		},
+		{
+			name: "invalid date",
+			args: args{
+				data: []byte(`"asdf"`),
+			},
+			want:    DateScheme{},
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := DateScheme{}
+			tt.wantErr(t, d.UnmarshalJSON(tt.args.data), fmt.Sprintf("UnmarshalJSON(%v)", tt.args.data))
+			assert.Equalf(t, tt.want, d, "MarshalJSON()")
+		})
+	}
+}
+
 func TestDateTimeScheme_MarshalJSON(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -62,7 +137,7 @@ func TestDateTimeScheme_UnmarshalJSON(t *testing.T) {
 			args: args{
 				data: []byte(`"2021-06-22T07:34:41+0000"`),
 			},
-			want:    DateTimeScheme{},
+			want:    DateTimeScheme(time.Date(2021, 6, 22, 7, 34, 41, 0, time.FixedZone("", 0))),
 			wantErr: assert.NoError,
 		},
 		{

6e1835e8f3299bd1de5aff1b847784230a6f4d80
Florian Kinder <florian.kinder@fankserver.com>
2025-05-15 20:35:59 +0900

    ✨ Added mypreferences (#362)
    Added mypreferences
    
    Took 1 hour 48 minutes

--- jira/internal/myself_impl_test.go
+++ jira/internal/myself_impl_test.go
@@ -3,12 +3,13 @@ package internal
 import (
 	"context"
 	"errors"
+	"net/http"
+	"testing"
+
 	model "github.com/ctreminiom/go-atlassian/v2/pkg/infra/models"
 	"github.com/ctreminiom/go-atlassian/v2/service"
 	"github.com/ctreminiom/go-atlassian/v2/service/mocks"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"testing"
 )
 
 func Test_internalMySelfImpl_Details(t *testing.T) {
@@ -193,6 +194,573 @@ func Test_NewMySelfService(t *testing.T) {
 
 				assert.NoError(t, err)
 				assert.NotEqual(t, got, nil)
+			}
+		})
+	}
+}
+
+func Test_internalMySelfImpl_Get(t *testing.T) {
+
+	type fields struct {
+		c       service.Connector
+		version string
+	}
+
+	type args struct {
+		ctx context.Context
+		key string
+	}
+
+	testCases := []struct {
+		name    string
+		fields  fields
+		args    args
+		on      func(*fields)
+		wantErr bool
+		Err     error
+	}{
+		{
+			name:   "when the api version is v3 with key parameter",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the api version is v3 without key parameter",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/3/mypreferences",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the api version is v2 with key parameter",
+			fields: fields{version: "2"},
+			args: args{
+				ctx: context.Background(),
+				key: "user.notifications.assignee.field",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/2/mypreferences?key=user.notifications.assignee.field",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the http request cannot be created",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, errors.New("error, unable to create the http request"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to create the http request"),
+		},
+
+		{
+			name:   "when the API call returns an error",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodGet,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, errors.New("error, unable to execute API call"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to execute API call"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			if testCase.on != nil {
+				testCase.on(&testCase.fields)
+			}
+
+			service := internalMySelfImpl{
+				c:       testCase.fields.c,
+				version: testCase.fields.version,
+			}
+
+			gotResult, gotResponse, err := service.Get(testCase.args.ctx, testCase.args.key)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.EqualError(t, err, testCase.Err.Error())
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, gotResponse, nil)
+				assert.NotEqual(t, gotResult, nil)
+			}
+		})
+	}
+}
+
+func Test_internalMySelfImpl_Set(t *testing.T) {
+
+	type fields struct {
+		c       service.Connector
+		version string
+	}
+
+	type args struct {
+		ctx   context.Context
+		key   string
+		value string
+	}
+
+	testCases := []struct {
+		name    string
+		fields  fields
+		args    args
+		on      func(*fields)
+		wantErr bool
+		Err     error
+	}{
+		{
+			name:   "when the api version is v3",
+			fields: fields{version: "3"},
+			args: args{
+				ctx:   context.Background(),
+				key:   "myuser.thousand.separator",
+				value: ",",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodPut,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"",
+					struct {
+						Value string `json:"value"`
+					}{
+						Value: ",",
+					}).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the api version is v2",
+			fields: fields{version: "2"},
+			args: args{
+				ctx:   context.Background(),
+				key:   "user.notifications.assignee.field",
+				value: "assignee",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodPut,
+					"rest/api/2/mypreferences?key=user.notifications.assignee.field",
+					"",
+					struct {
+						Value string `json:"value"`
+					}{
+						Value: "assignee",
+					}).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the key is not provided",
+			fields: fields{version: "3"},
+			args: args{
+				ctx:   context.Background(),
+				key:   "",
+				value: ",",
+			},
+			on:      nil,
+			wantErr: true,
+			Err:     model.ErrNoKeyError,
+		},
+
+		{
+			name:   "when the http request cannot be created",
+			fields: fields{version: "3"},
+			args: args{
+				ctx:   context.Background(),
+				key:   "myuser.thousand.separator",
+				value: ",",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodPut,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"",
+					struct {
+						Value string `json:"value"`
+					}{
+						Value: ",",
+					}).
+					Return(&http.Request{}, errors.New("error, unable to create the http request"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to create the http request"),
+		},
+
+		{
+			name:   "when the API call returns an error",
+			fields: fields{version: "3"},
+			args: args{
+				ctx:   context.Background(),
+				key:   "myuser.thousand.separator",
+				value: ",",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodPut,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"",
+					struct {
+						Value string `json:"value"`
+					}{
+						Value: ",",
+					}).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					&map[string]interface{}{}).
+					Return(&model.ResponseScheme{}, errors.New("error, unable to execute API call"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to execute API call"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			if testCase.on != nil {
+				testCase.on(&testCase.fields)
+			}
+
+			service := internalMySelfImpl{
+				c:       testCase.fields.c,
+				version: testCase.fields.version,
+			}
+
+			gotResult, gotResponse, err := service.Set(testCase.args.ctx, testCase.args.key, testCase.args.value)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.EqualError(t, err, testCase.Err.Error())
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, gotResponse, nil)
+				assert.NotEqual(t, gotResult, nil)
+			}
+		})
+	}
+}
+
+func Test_internalMySelfImpl_Delete(t *testing.T) {
+
+	type fields struct {
+		c       service.Connector
+		version string
+	}
+
+	type args struct {
+		ctx context.Context
+		key string
+	}
+
+	testCases := []struct {
+		name    string
+		fields  fields
+		args    args
+		on      func(*fields)
+		wantErr bool
+		Err     error
+	}{
+		{
+			name:   "when the api version is v3",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodDelete,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					nil).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the api version is v2",
+			fields: fields{version: "2"},
+			args: args{
+				ctx: context.Background(),
+				key: "user.notifications.assignee.field",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodDelete,
+					"rest/api/2/mypreferences?key=user.notifications.assignee.field",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					nil).
+					Return(&model.ResponseScheme{}, nil)
+
+				fields.c = client
+			},
+			wantErr: false,
+			Err:     nil,
+		},
+
+		{
+			name:   "when the key is not provided",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "",
+			},
+			on:      nil,
+			wantErr: true,
+			Err:     model.ErrNoKeyError,
+		},
+
+		{
+			name:   "when the http request cannot be created",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodDelete,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, errors.New("error, unable to create the http request"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to create the http request"),
+		},
+
+		{
+			name:   "when the API call returns an error",
+			fields: fields{version: "3"},
+			args: args{
+				ctx: context.Background(),
+				key: "myuser.thousand.separator",
+			},
+			on: func(fields *fields) {
+
+				client := mocks.NewConnector(t)
+
+				client.On("NewRequest",
+					context.Background(),
+					http.MethodDelete,
+					"rest/api/3/mypreferences?key=myuser.thousand.separator",
+					"", nil).
+					Return(&http.Request{}, nil)
+
+				client.On("Call",
+					&http.Request{},
+					nil).
+					Return(&model.ResponseScheme{}, errors.New("error, unable to execute API call"))
+
+				fields.c = client
+			},
+			wantErr: true,
+			Err:     errors.New("error, unable to execute API call"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			if testCase.on != nil {
+				testCase.on(&testCase.fields)
+			}
+
+			service := internalMySelfImpl{
+				c:       testCase.fields.c,
+				version: testCase.fields.version,
+			}
+
+			gotResponse, err := service.Delete(testCase.args.ctx, testCase.args.key)
+
+			if testCase.wantErr {
+
+				if err != nil {
+					t.Logf("error returned: %v", err.Error())
+				}
+
+				assert.EqualError(t, err, testCase.Err.Error())
+			} else {
+
+				assert.NoError(t, err)
+				assert.NotEqual(t, gotResponse, nil)
 			}
 		})
 	}
